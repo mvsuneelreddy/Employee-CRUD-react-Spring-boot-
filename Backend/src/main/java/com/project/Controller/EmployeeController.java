8e007a40c581f83085b2d3385606dbe0e5ff165c
M V Suneel Reddy <msuneelreddy0@gmail.com>
2025-05-27 10:37:49 +0530

    added delete mapping

--- Backend/src/main/java/com/project/Controller/EmployeeController.java
+++ Backend/src/main/java/com/project/Controller/EmployeeController.java
@@ -2,8 +2,14 @@ package com.project.Controller;
 
 import com.project.Entity.Employee;
 import com.project.service.Employeeservice;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -14,6 +20,23 @@ public class EmployeeController {
     @PostMapping("/employee")
     public Employee postemployee(@RequestBody Employee employee){
         return emloyeeservice.Postemployee(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getallemployees(){
+        return emloyeeservice.getallemployees();
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<?> deleteemployee(@PathVariable Long id){
+        try {
+            emloyeeservice.deleteemployee(id);
+            return new ResponseEntity<>("Employee with id " + id + " deleted sucessfully " , HttpStatus.OK);
+        }
+        catch (EntityNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
     }
 
 }
